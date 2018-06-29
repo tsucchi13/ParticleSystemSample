01a6673dcefe3b37abe57981c8b7b32a7bed3dd6
Tsubasa Tsuchiya <t.tsuchiya83@gmai.com>
2018-06-29 20:59:44 +0900

    hoge commit

---
+++ Assets/hoge.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("hoge");
+		Debug.Log ("hogehoge");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
